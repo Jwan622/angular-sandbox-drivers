279bbb9de7c05ff76692b2c244036a6f71b909e1
Jeffrey Wan <Jwan622@gmail.com>
2015-07-16 15:29:02 +0100

    finished angular tutorial

--- app/js/services.js
+++ app/js/services.js
@@ -5,21 +5,21 @@ angular.module('F1FeederApp.services', [])
 
     ergastAPI.getDrivers = function() {
       return $http({
-        method: 'JSONP', 
+        method: 'JSONP',
         url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
       });
     }
 
     ergastAPI.getDriverDetails = function(id) {
       return $http({
-        method: 'JSONP', 
+        method: 'JSONP',
         url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
       });
     }
 
     ergastAPI.getDriverRaces = function(id) {
       return $http({
-        method: 'JSONP', 
+        method: 'JSONP',
         url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
       });
     }
